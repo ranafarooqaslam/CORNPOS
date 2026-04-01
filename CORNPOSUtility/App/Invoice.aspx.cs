c719159017ee2b9cc4e7bc44f25195375105e2d8
hp <hp@DESKTOP-Q07NSGI>
2026-04-01 12:48:33 +0500

    Worked on Invoice Date change and voucher date issue

--- CORNPOSUtility/App/Invoice.aspx.cs
+++ CORNPOSUtility/App/Invoice.aspx.cs
@@ -67,7 +67,15 @@ public partial class Invoice : System.Web.UI.Page
                         {
                             if (txtNewDate.Text.Length > 0)
                             {
-                                strQuery = string.Format("USE {0} UPDATE SALE_INVOICE_MASTER SET DOCUMENT_DATE = '" + Convert.ToDateTime(txtNewDate.Text).ToString("yyyy-MM-dd") + "' ,LASTUPDATE_DATE = '" + System.DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.fff") + "' WHERE SALE_INVOICE_ID=" + Convert.ToInt64(gvr.Cells[1].Text) + " UPDATE GL_MASTER SET VOUCHER_DATE = '" + Convert.ToDateTime(txtNewDate.Text).ToString("yyyy-MM-dd") + "' WHERE INVOICE_TYPE = 13 AND INVOICE_ID = " + Convert.ToInt64(gvr.Cells[1].Text) + " UPDATE [LEDGER] SET LEDGER_DATE = '" + Convert.ToDateTime(txtNewDate.Text).ToString("yyyy-MM-dd") + "' WHERE DOCUMENT_NO = " + Convert.ToInt64(gvr.Cells[1].Text), ddlDB.SelectedItem);
+                                if (Convert.ToDateTime(txtNewDate.Text).Month == Convert.ToDateTime(txtDate.Text).Month)//If new date month is same as of old date
+                                {
+                                    strQuery = string.Format("USE {0} UPDATE SALE_INVOICE_MASTER SET DOCUMENT_DATE = '" + Convert.ToDateTime(txtNewDate.Text).ToString("yyyy-MM-dd") + "' ,LASTUPDATE_DATE = '" + System.DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.fff") + "' WHERE SALE_INVOICE_ID=" + Convert.ToInt64(gvr.Cells[1].Text) + " UPDATE GL_MASTER SET VOUCHER_DATE = '" + Convert.ToDateTime(txtNewDate.Text).ToString("yyyy-MM-dd") + "' WHERE INVOICE_TYPE = 13 AND INVOICE_ID = " + Convert.ToInt64(gvr.Cells[1].Text) + " UPDATE [LEDGER] SET LEDGER_DATE = '" + Convert.ToDateTime(txtNewDate.Text).ToString("yyyy-MM-dd") + "' WHERE DOCUMENT_NO = " + Convert.ToInt64(gvr.Cells[1].Text), ddlDB.SelectedItem);
+                                }
+                                else//if new date month is different than old month then change voucher nos according to new date month.
+                                {
+                                    UpdateVoucherNo(Convert.ToDateTime(txtNewDate.Text), Convert.ToInt64(gvr.Cells[1].Text));
+                                    strQuery = string.Format("USE {0} UPDATE SALE_INVOICE_MASTER SET DOCUMENT_DATE = '" + Convert.ToDateTime(txtNewDate.Text).ToString("yyyy-MM-dd") + "' ,LASTUPDATE_DATE = '" + System.DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.fff") + "' WHERE SALE_INVOICE_ID=" + Convert.ToInt64(gvr.Cells[1].Text) + " UPDATE [LEDGER] SET LEDGER_DATE = '" + Convert.ToDateTime(txtNewDate.Text).ToString("yyyy-MM-dd") + "' WHERE DOCUMENT_NO = " + Convert.ToInt64(gvr.Cells[1].Text), ddlDB.SelectedItem);
+                                }
                             }
                         }
                         if (strQuery.Length > 0)
@@ -90,10 +98,10 @@ public partial class Invoice : System.Web.UI.Page
                 }
             }
         }
-        if(count > 0)
+        if (count > 0)
         {
             lblError.ForeColor = System.Drawing.Color.Green;
-            if(rblType.SelectedItem.Value == "1")
+            if (rblType.SelectedItem.Value == "1")
             {
                 lblError.Text = "Selected Invoice(s) rollbacked successfully.";
             }
@@ -108,7 +116,7 @@ public partial class Invoice : System.Web.UI.Page
             lblError.ForeColor = System.Drawing.Color.Red;
             lblError.Text = "No database selected or Some error occured.";
         }
-    }    
+    }
     protected void ddlDB_SelectedIndexChanged(object sender, EventArgs e)
     {
         string connString = conStringCorn;
@@ -160,9 +168,9 @@ public partial class Invoice : System.Web.UI.Page
     }
 
     protected void btnLoad_Click(object sender, EventArgs e)
-    {        
+    {
         LoadInvoice();
-    }    
+    }
     private void LoadInvoice()
     {
         if (ddlLocation.Items.Count > 0)
@@ -192,7 +200,7 @@ public partial class Invoice : System.Web.UI.Page
         txtNewDate.Visible = false;
         lblNewDate.Visible = false;
         btnUpdate.Text = "Rollback Invocie";
-        if(rblType.SelectedItem.Value == "2")
+        if (rblType.SelectedItem.Value == "2")
         {
             btnUpdate.Text = "Change Date";
             txtNewDate.Visible = true;
@@ -203,5 +211,144 @@ public partial class Invoice : System.Web.UI.Page
     protected void ddlLocation_SelectedIndexChanged(object sender, EventArgs e)
     {
         LoadInvoice();
+    }
+    private void UpdateVoucherNo(DateTime NewDate, long INVOICE_ID)
+    {
+        string strQuery = string.Empty;
+        DataTable dtVoucherNoOld = new DataTable();
+        string VoucherNoNew = GetVoucherNo(Convert.ToInt32(ddlLocation.SelectedValue), NewDate);
+        using (SqlConnection con = new SqlConnection(conStringCorn))
+        {
+            con.Open();
+            using (SqlCommand cmd = new SqlCommand(string.Format("USE {0} SELECT VOUCHER_NO FROM GL_MASTER WHERE INVOICE_TYPE = 13 AND INVOICE_ID = " + INVOICE_ID, ddlDB.SelectedItem), con))
+            {
+                using (IDataReader dr = cmd.ExecuteReader())
+                {
+                    dtVoucherNoOld.Load(dr);
+                }
+            }
+            con.Close();
+        }
+        string VoucherNoOld = dtVoucherNoOld.Rows[0][0].ToString();
+
+        using (SqlConnection con = new SqlConnection(conStringCorn))
+        {
+            strQuery = string.Format("USE {0} UPDATE GL_MASTER SET VOUCHER_DATE = '" + NewDate.ToString("yyyy-MM-dd") + "' ,VOUCHER_NO = '" + VoucherNoNew + "' WHERE VOUCHER_NO ='" + VoucherNoOld + "' AND INVOICE_TYPE = 13 AND INVOICE_ID = " + INVOICE_ID + " UPDATE GL_DETAIL SET VOUCHER_NO ='" + VoucherNoNew + "' WHERE VOUCHER_NO='" + VoucherNoOld + "' AND VOUCHER_TYPE_ID= 16 AND DISTRIBUTOR_ID=" + Convert.ToInt32(ddlLocation.SelectedValue), ddlDB.SelectedItem);
+            if (strQuery.Length > 0)
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(strQuery, con))
+                {
+                    if (cmd.ExecuteNonQuery() > 0)
+                    {
+                    }
+                }
+                con.Close();
+            }
+        }
+    }
+
+    public string GetVoucherNo(int SelectedLocation, DateTime pMonth)
+    {
+        string conString2 = "server=" + System.Configuration.ConfigurationManager.AppSettings["server"].ToString()
+            + ";uid=" + System.Configuration.ConfigurationManager.AppSettings["uid"].ToString()
+            + ";pwd=" + System.Configuration.ConfigurationManager.AppSettings["pwd"].ToString()
+            + ";database=" + ddlDB.Text;
+        DataSet ds = new DataSet();
+        string VoucherNo = string.Empty;
+        try
+        {
+            using (SqlConnection con = new SqlConnection(conString2))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = con;
+                    cmd.CommandText = "UspSelectMaxVoucherNo";
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    IDataParameterCollection pparams = cmd.Parameters;
+
+                    IDataParameter parameter = new SqlParameter() { ParameterName = "@Distributor_id", DbType = DbType.Int32, Value = SelectedLocation };
+                    pparams.Add(parameter);
+
+                    parameter = new SqlParameter() { ParameterName = "@Document_TypeId", DbType = DbType.Int32, Value = 16 };
+                    pparams.Add(parameter);
+
+                    parameter = new SqlParameter() { ParameterName = "@Month", DbType = DbType.DateTime, Value = pMonth };
+                    pparams.Add(parameter);
+
+                    IDbDataAdapter da = new SqlDataAdapter();
+                    da.SelectCommand = cmd;
+                    da.Fill(ds);
+                    VoucherNo = ds.Tables[0].Rows[0][0].ToString();
+                }
+                con.Close();
+            }
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
+        if (VoucherNo.Length == 1)
+        {
+            if (pMonth.Month.ToString().Length == 1)
+            {
+                VoucherNo = "0" + pMonth.Month.ToString() + pMonth.Year.ToString().Substring(2, 2) + "-0000" + VoucherNo;
+            }
+            else
+            {
+                VoucherNo = pMonth.Month.ToString() + pMonth.Year.ToString().Substring(2, 2) + "-0000" + VoucherNo;
+            }
+
+        }
+        else if (VoucherNo.Length == 2)
+        {
+            if (pMonth.Month.ToString().Length == 1)
+            {
+                VoucherNo = VoucherNo = "0" + pMonth.Month.ToString() + pMonth.Year.ToString().Substring(2, 2) + "-000" + VoucherNo;
+            }
+            else
+            {
+                VoucherNo = pMonth.Month.ToString() + pMonth.Year.ToString().Substring(2, 2) + "-000" + VoucherNo;
+            }
+
+        }
+        else if (VoucherNo.Length == 3)
+        {
+            if (pMonth.Month.ToString().Length == 1)
+            {
+                VoucherNo = "0" + pMonth.Month.ToString() + pMonth.Year.ToString().Substring(2, 2) + "-00" + VoucherNo;
+            }
+            else
+            {
+                VoucherNo = pMonth.Month.ToString() + pMonth.Year.ToString().Substring(2, 2) + "-00" + VoucherNo;
+            }
+
+        }
+        else if (VoucherNo.Length == 4)
+        {
+            if (pMonth.Month.ToString().Length == 1)
+            {
+                VoucherNo = "0" + pMonth.Month.ToString() + pMonth.Year.ToString().Substring(2, 2) + "-0" + VoucherNo;
+            }
+            else
+            {
+                VoucherNo = pMonth.Month.ToString() + pMonth.Year.ToString().Substring(2, 2) + "-0" + VoucherNo;
+            }
+
+        }
+        else
+        {
+            if (pMonth.Month.ToString().Length == 1)
+            {
+                VoucherNo = "0" + pMonth.Month.ToString() + pMonth.Year.ToString().Substring(2, 2) + "-" + VoucherNo;
+            }
+            else
+            {
+                VoucherNo = pMonth.Month.ToString() + pMonth.Year.ToString().Substring(2, 2) + "-" + VoucherNo;
+            }
+        }
+        return VoucherNo;
     }
 }
